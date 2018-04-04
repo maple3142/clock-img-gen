ddeae61fe3021bc7a432fc5ea5f17e0e7547bb1b
maple3142 <kirby741852963@gmail.com>
2018-04-04 17:57:57 +0800

    add gzip & use stream

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Canvas = require('canvas')
+const compression = require('compression')
 const app = require('express')()
 const renderClock = require('./clock')
+
+app.use(compression())
 
 app.get('/clock.png', (req, res) => {
 	const r = (req.query.size || 1000) / 2
 	const cvs = new Canvas(r * 2, r * 2)
 	renderClock(cvs.getContext('2d'), r)
 	res.set('Content-Type', 'image/png')
-	res.send(cvs.toBuffer())
+	cvs.pngStream().pipe(res)
 })
 
 const PORT = process.env.PORT || 7894
